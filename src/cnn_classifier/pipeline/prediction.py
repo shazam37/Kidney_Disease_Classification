0346c7af071ecab93abb8b3aeef6d29e495e530f
shazam <shazajmal37@gmail.com>
2024-03-02 16:23:37 +0530

    add predict

--- src/cnn_classifier/pipeline/prediction.py
+++ src/cnn_classifier/pipeline/prediction.py
@@ -35,17 +35,17 @@ class PredictionPipeline:
 
         if result[0] == 0:
             prediction = 'Cyst'
-            return {"predicted_image": prediction}
+            return [{"predicted_image": prediction}]
         elif result[0] == 1:
             prediction = 'Normal'
-            return {"predicted_image": prediction}
+            return [{"predicted_image": prediction}]
         elif result[0] == 2:
             prediction = 'Stone'
-            return {"predicted_image": prediction}
+            return [{"predicted_image": prediction}]
         elif result[0] == 3:
             prediction = 'Tumor'
-            return {"predicted_image": prediction}
+            return [{"predicted_image": prediction}]
         else:
             prediction = 'null'
-            return {'predicted_image': prediction}
+            return [{'predicted_image': prediction}]
         
