51911e63872ef159219817a26a31c7ece9989671
shazam <shazajmal37@gmail.com>
2024-03-02 16:20:40 +0530

    update prediction

--- src/cnn_classifier/pipeline/prediction.py
+++ src/cnn_classifier/pipeline/prediction.py
@@ -20,17 +20,29 @@ class PredictionPipeline:
 
         # decide and write
 
+        # if result[0] == 0:
+        #     prediction = 'Cyst'
+        #     return {"predicted_image": prediction, 'actual_image': imagename , 'label': result[0]}
+        # elif result[0] == 1:
+        #     prediction = 'Normal'
+        #     return {"predicted_image": prediction, 'actual_image': imagename , 'label': result[0]}
+        # elif result[0] == 2:
+        #     prediction = 'Stone'
+        #     return {"predicted_image": prediction, 'actual_image': imagename , 'label': result[0]}
+        # elif result[0] == 3:
+        #     prediction = 'Tumor'
+        #     return {"predicted_image": prediction, 'actual_image': imagename , 'label': result[0]}
+
         if result[0] == 0:
             prediction = 'Cyst'
-            return {"predicted_image": prediction, 'actual_image': imagename , 'label': result[0]}
+            return {"predicted_image": prediction}
         elif result[0] == 1:
             prediction = 'Normal'
-            return {"predicted_image": prediction, 'actual_image': imagename , 'label': result[0]}
+            return {"predicted_image": prediction}
         elif result[0] == 2:
             prediction = 'Stone'
-            return {"predicted_image": prediction, 'actual_image': imagename , 'label': result[0]}
+            return {"predicted_image": prediction}
         elif result[0] == 3:
             prediction = 'Tumor'
-            return {"predicted_image": prediction, 'actual_image': imagename , 'label': result[0]}
-
+            return {"predicted_image": prediction}
         
